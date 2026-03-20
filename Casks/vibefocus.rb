86fe825ae890b825da7891342c21f610a0769ec3
CC11001100 <cc11001100@qq.com>
2026-03-20 23:02:22 +0800

    Fix SHA256 for v0.0.4

--- Casks/vibefocus.rb
+++ Casks/vibefocus.rb
@@ -1,6 +1,6 @@
 cask "vibefocus" do
   version "0.0.4"
-  sha256 "ee8933281176f8219951b3c4c72e1723d58affe950d140c5912ae445bf6f67ec"
+  sha256 "615f5c81dcc0f79815be2cbf09533f8b63c5000618a45807a521aa7eb6e6904c"
 
   url "https://github.com/vibe-coding-labs/vibe-focus/releases/download/v#{version}/VibeFocus-#{version}-macos.zip"
   name "VibeFocus"
